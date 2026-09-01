bf53f7261c8e984068299dd6e45bc13cc1bdaaec
Laserrag <hellokishp.gmail.com>
2026-09-01 20:41:16 +0530

    k

--- Arguments.py
+++ Arguments.py
@@ -2,3 +2,11 @@
 def greet(name, age):
     print("Hello", name, ", you are", age, "years old.")
 #Recursion-when a calls itself.
+def square(num):
+    """
+    Returns the square of a number.
+    """
+    return num * num
+
+print(square(5))
+print(square.__doc__)

8f43fcd467a86e0ce5fbdc1e57efa88a794f67d1
Laserrag <hellokishp.gmail.com>
2026-09-01 20:41:45 +0530

    k

--- Arguments.py
+++ Arguments.py
@@ -10,3 +10,24 @@ def square(num):
 
 print(square(5))
 print(square.__doc__)
+
+#Assignment 1
+def factorial(n):
+    """This is a recursive function to find the factorial of an integer."""
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+print(factorial(5))
+print(factorial.__doc__)
+
+#Assignment 2
+def total_calc(bill_amount, tip_percentage):
+    total = bill_amount * (1 + 0.01 * tip_percentage)
+    print("The total bill amount is", total, "dollars.")
+    total = round(total, 2) 
+    print("The total bill amount rounded to 2 decimal places is", total, "dollars.")
+    print("Please pay", total, "dollars.")
+
+total_calc(150,20)
